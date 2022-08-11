5148a08d9708ab0a25216f3b34a3f6a9c41379e3
Irene131313 <irinabadytcik@gmail.com>
2022-08-11 22:39:48 +0300

    init: start towel task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
+const arr = [[ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ]];
+module.exports = function towelSort (arr) {
+if (arr === undefined) {
+ return [] 
+ }
 
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+ else if (arr.length === 0) {
+  return []
+  
+ }
+ else {
+  let result =  arr.reduce((acc, cur, i) => {
+    cur.sort((a, b) => !(i & 1) ? a - b : b - a).map(e => acc.push(e));
+    return acc;
+  }, []);
+  return result; 
+ }
+};
